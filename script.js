832979a88db344016697945a1b2fb638a7386c17
Muzammel Haque <mhktushar018@gmail.com>
2021-02-08 17:08:28 +0600

    some design added

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
-.then(res => res.json())
-.then(data => displayNames(data))
+document.getElementById('search-btn').addEventListener('click', ()=>{
+    const inputBtn = document.getElementById('input-btn').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${inputBtn}`)
+    .then(res => res.json())
+    .then(data => displayNames(data))
 
-const displayNames = mealItems =>{
-        const displayItems = document.getElementById('display-items');
+    const displayNames = mealItems =>{
+            const displayItems = document.getElementById('display-items');
 
-        const mealItemNames = mealItems.meals;
-        for (let i = 0; i < 8; i++) {
-            const shownNames = mealItemNames[i];
-            console.log(shownNames)
-            const mealsDiv =document.createElement('div');
-            mealsDiv.className ='meals';
-            const mealsInfo = `
-            <img class="menu-img" src="${shownNames.strMealThumb}" alt="">
-            <h6 class="menu-name">${shownNames.strMeal}</h6>
-        `
-        mealsDiv.innerHTML = mealsInfo;
-        displayItems.appendChild(mealsDiv);
-        }
-       
-}
+            const mealItemNames = mealItems.meals;
+            mealItemNames.forEach(shownNames => {
+                console.log(shownNames)
+                    const mealsDiv =document.createElement('div');
+                    mealsDiv.className ='meals';
+                    const mealsInfo = `
+                    <img class="menu-img" src="${shownNames.strMealThumb}" alt="">
+                    <h6 class="menu-name">${shownNames.strMeal}</h6>
+                `
+                mealsDiv.innerHTML = mealsInfo;
+                displayItems.appendChild(mealsDiv);
+            });
+        
+    }
+})
+
